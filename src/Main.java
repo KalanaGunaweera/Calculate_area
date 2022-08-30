4f8e39a1cb843ee48ee6122b34ba1fbc1d522af2
Kalana <kalanagunaweera94@gamail.com>
2022-08-30 22:54:41 +0530

    updated

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        SquareCalculator cal =new SquareCalculator();
+        cal.setHeight(5);
+        cal.setWidth(6);
+        System.out.println("CalculatorSquar "+cal.getWidth()*cal.getHeight());
+
+
+
+
+    }
+}
---
+++ src/SquareCalculator.java
@@ -0,0 +1,20 @@
+public class SquareCalculator {
+    private int height;
+    private int width;
+
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+
+
+}
